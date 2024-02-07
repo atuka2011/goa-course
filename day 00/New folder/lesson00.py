61bc598f378a756c7a1ad38b1dc09b6ce12bddb2
atuka2011 <atukashekriladze@gmail.com>
2024-02-07 07:27:33 -0800

    first commit

---
+++ day 00/New folder/lesson00.py
@@ -0,0 +1,232 @@
+# name = "atuka"
+# username = "shekriladze,"
+# age = " I'm 12 years old,"
+# higt = "I'm 1.75 metres,"
+# hobbies ="My hobby basketball game."
+
+
+# print(name + " " + username + " " + age + " " + higt + " " + hobbies  ) 
+
+
+
+# from turtle import *
+# #we start drawing a home
+# #we start drawing a square
+
+# speed(5)
+# width(6)
+ 
+# color("brown")
+
+# forward(150)
+# left(90)
+# forward(150)
+# left(90)
+# forward(150)
+# left(90)
+# forward(150)
+# left(90)
+
+# #end of square       
+
+# #drawing a door
+
+
+# forward(50)
+# color("green")
+# begin_fill()
+# left(90)
+# forward(100) #height of a door
+# right(90)   
+# forward(50)
+# right(90)
+# forward(100)
+# end_fill()
+
+# penup()
+# goto(150,150)
+# pendown()
+
+# color("blue")
+# begin_fill()
+# right(150)
+# forward(150)
+# left(120)
+# forward(150)
+# end_fill()
+
+
+
+
+
+from turtle import *
+#we start drawing a tree
+#we start drawing a square 
+begin_fill()
+color("brown")  
+speed(5)
+width(5)
+
+forward(50)
+left(90)
+forward(120)
+left(90)
+forward(50)
+left(90)
+forward(120)
+end_fill()
+# we finished drawing a square 
+# we start drawing a leafs 
+penup()
+goto(50,120)
+pendown()
+
+begin_fill()
+color("green")
+
+left(90)
+forward(40)
+left(90)
+forward(100)
+left(90)
+forward(130)
+left(90)
+forward(100)
+left(90)
+forward(130)
+end_fill()
+#we finished drawing leaf
+#we start drawing apple1
+begin_fill()
+color("red")
+
+penup()
+goto(50,200)
+pendown()
+
+
+forward(10)
+left(90)
+forward(10)
+left(90)
+forward(10)
+left(90)
+forward(10)
+left(90)
+end_fill()
+#we finished drawing apple1
+#we start drawing apple2
+begin_fill()
+
+color("red")
+
+penup()
+goto(65,130)
+pendown()
+
+forward(10)
+left(90)
+forward(10)
+left(90)
+forward(10)
+left(90)
+forward(10)
+left(90)
+end_fill()
+#we finished drawing apple 2
+#we start  drawing apple 3
+begin_fill()
+color("red")
+
+penup()
+goto(10, 160)
+pendown()
+
+forward(10)
+left(90)
+forward(10)
+left(90)
+forward(10)
+left(90)
+forward(10)
+left(90)
+
+end_fill()
+
+#we finished drawing apple 3
+
+
+
+#we finished drawing a tree
+
+exitonclick()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
