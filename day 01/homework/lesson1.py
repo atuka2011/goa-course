538fc97d37b8f2d223da0d955d364b8a60ca7bc0
atuka2011 <atukashekriladze@gmail.com>
2024-02-09 04:04:18 -0800

    second commit

---
+++ day 01/homework/lesson1.py
@@ -0,0 +1,8 @@
+name = "atuka"
+username = "shekriladze,"
+age = " I'm 12 years old,"
+hight = "I'm 1.75 metres,"
+hobbies ="My hobby basketball game."
+
+
+print(name + " " + username + " " + age + " " + hight + " " + hobbies  )
